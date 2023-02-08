1f015da0d4464cb21156a10ed9356fdd9c9bf099
Matveev Aleksander <matveev.alexander96@gmail.com>
2023-02-08 18:27:14 +0300

    Remake FindByNumOrTrackFragment.kt using compose

--- app/src/main/java/com/example/l_post_tracking/presentation_compose/MainScreen.kt
+++ app/src/main/java/com/example/l_post_tracking/presentation_compose/MainScreen.kt
@@ -4,12 +4,15 @@ import android.graphics.Color.rgb
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,8 +22,7 @@ import com.example.l_post_tracking.R
 @Composable
 fun MainScreen() {
     Box(
-        modifier = Modifier
-            .background(
+        modifier = Modifier.background(
                 brush = Brush.linearGradient(
                     listOf(
                         Color(red = 91, green = 37, blue = 153),
@@ -28,18 +30,24 @@ fun MainScreen() {
                     )
                 )
             )
-            .width(100.dp)
-            .height(100.dp)
     ) {
-        Row() {
-            Image(painter = painterResource(id = R.drawable.main_page_logo), contentDescription = null)
-            Spacer(modifier = Modifier.weight(1f))
-            Text(text = "IIIIII")
-            Column() {
-                Text(text = "ffffff")
-                Button(onClick = {}) {
-                    Text(text = "hhhhhhhhh")
-                }
+        MainScreenHeader()
+    }
+}
+
+@Composable
+fun MainScreenHeader() {
+    Row() {
+        Image(
+            painter = painterResource(id = R.drawable.main_page_logo),
+            contentDescription = null,
+            modifier = Modifier.width(150.dp)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Column() {
+            Text(text = "8 800 700–1006")
+            Button(onClick = {}) {
+                Text(text = "Новый поиск")
             }
         }
     }
